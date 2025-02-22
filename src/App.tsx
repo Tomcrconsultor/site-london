736ec3fc17e072c04937858b32bff8956a3e559b
thpmaz <tomcrconsultor@gmail.com>
2025-02-22 15:50:04 -0300

    fix: Ajustes para corrigir o deploy na Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import PoliticaDePrivacidade from './components/PoliticaDePrivacidade';
 import TermosDeUso from './components/TermosDeUso';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/politica-de-privacidade" element={<PoliticaDePrivacidade />} />
         <Route path="/termos-de-uso" element={<TermosDeUso />} />
+        <Route path="*" element={<Index />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
