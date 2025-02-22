73cf6c42516226d23efff0ac3752649c51500a9b
thpmaz <tomcrconsultor@gmail.com>
2025-02-22 15:44:25 -0300

    fix: Ajustes na configuração do Vite para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { compression } from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteImageOptimizer({
-      jpg: {
-        quality: 80
-      },
-      jpeg: {
-        quality: 80
-      },
-      png: {
-        quality: 80
-      },
-      webp: {
-        quality: 80
-      }
-    }),
     compression()
   ],
   resolve: {
